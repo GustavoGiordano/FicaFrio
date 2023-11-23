0399ad9c7a6069b944af32e6b783a0601b914dbb
Ryan De Souza Tomaz <80019514+Ryan-STT@users.noreply.github.com>
2023-11-22 22:33:09 -0300

    Footer_css

--- src/Components/Rodape/Rodape.jsx
+++ src/Components/Rodape/Rodape.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Logo from '../../assets/Logo.png'
 import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 import './Rodape.css'
 
@@ -12,21 +14,22 @@ function Rodape() {
 
     return (
         <>
-            <div>
+            <div className='rodape'>
                 <Row>
-                    <Col md={4}>
-                        <ul>
-                            <li><img src={Logo} alt="LogoTipo" /></li>
+                    <Col className='rodape_col' md={5}>
+                        <ul className='LogoTipo'>
+                            <img src={Logo} alt="LogoTipo" />
+                            <hr className='linhadivisoria'/>
                             <h1>Redes socias:</h1>
                         </ul>
                         <div className='social-links'>
-                            <FontAwesomeIcon icon="fa-brands fa-x-twitter" />
-                            <FontAwesomeIcon icon="fa-brands fa-x-twitter" />
-                            <FontAwesomeIcon icon="fa-brands fa-square-instagram" />
-                            <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+                            <FontAwesomeIcon icon="fa-brands fa-x-twitter"/>
+                            <FontAwesomeIcon icon="fa-brands fa-x-twitter"/>
+                            <FontAwesomeIcon icon="fa-brands fa-square-instagram"/>
+                            <FontAwesomeIcon icon="fa-brands fa-linkedin"/>
                         </div>
                     </Col>
-                    <Col md={4}>
+                    <Col className='rodape_LinksUteis' md={4}>
                         <h1>Links Uteis</h1>
                         <ul>
                             <li>Soluções</li>
@@ -36,15 +39,35 @@ function Rodape() {
                             <li>Fale Conosco</li>
                         </ul>
                     </Col>
-                    <Col md={4}>
+                    <Col className='rodape_BoletimNoticias' md={3}>
                         <h1>Boletim de Notícias</h1>
                         <p>Acompanhe o projeto e receba dicas de organização</p>
-                        Concordo em receber noticias da equipe Frio
+                        <Form className='Formulario'>
+                            <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                                <Row>
+                                    <Col className='Email' sm="5">
+                                        <Form.Control type="email" placeholder="Insira seu Email" />
+                                    </Col>
+                                    <Col className='Botao'>
+                                        <Button as="input" type="submit" value="Inscreva-se" />{''}
+                                    </Col>
+                                </Row>
+                            </Form.Group>
+                            {['checkbox'].map((type) => (
+                                <div div key={`default-${type}`} className="mb-3">
+                                    <Form.Check className='CheckBoxNoticias' // prettier-ignore
+                                        type={type}
+                                        id={`default-${type}`}
+                                        label={`Concordo em receber noticias da equipe Frio`}
+                                    />
+                                </div>
+                            ))}
+                        </Form>
                     </Col>
                 </Row>
 
                 <div className='direitos-reservados'>
-                    <h1>2023 Fica Fri.io - Todos direitos reservados</h1>
+                    <h1>@2023 Fica Fri.io - Todos direitos reservados</h1>
                 </div>
             </div>
 
