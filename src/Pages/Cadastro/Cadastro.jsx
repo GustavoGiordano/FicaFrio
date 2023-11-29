1188852128b103beaa278f6749ab540bf89cf72f
GustavoGiordano <gustavogiordano7@gmail.com>
2023-11-28 21:01:12 -0300

    Tela de Login e Cadastro

--- src/Pages/Cadastro/Cadastro.jsx
+++ src/Pages/Cadastro/Cadastro.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
-import './Cadastro.css'
+import Logo from '../../assets/Logo.png'
+import { Card, Button } from 'react-bootstrap';
 
 const Cadastro = () => {
   return (
-    <>
-    cadastro
-    </>
+    /*<div className="card">
+      <img className='logo' src={Logo} alt="Logo" />
+      <div className="card-content">
+        <h2>Pronto para ficar frio?</h2>
+        <div className="caixa1">Senha</div>
+        <div className="caixa1">Senha</div>
+        <div className="caixa2">Esqueceu sua senha?</div>
+        <Button>Continuar</Button>
+        <p className='link_cadastro'>Não possui conta? <a href="">Clique aqui e faça seu cadastro</a></p>
+      </div>
+    </div >*/
+
+    <div className="fundoCadastro">
+      <Card className="text-center">
+
+        <Card.Link href="/" className='logo'>
+          <img
+            src={Logo}
+            alt="Logo"
+            width={100}
+          />
+        </Card.Link>
+
+        <Card.Title><strong>Olá! Seja bem vindo(a) ao Fica Frio.</strong></Card.Title>
+
+        <Card.Text className='textoFria'>
+          <strong>Meu nome é Fri.ia, sou a sua companheira que vai resfriar o seu uso nesse site</strong>
+        </Card.Text>
+
+        <Button className='continuar'>
+          <strong>Continuar</strong>
+        </Button>
+
+        <Card.Text className='naoPossuiConta'>
+          Já tem uma conta? -
+          <Card.Link href="login">
+            Clique aqui e faça o login
+          </Card.Link>
+        </Card.Text>
+
+      </Card>
+    </div>
   )
 }
 
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
+import Logo from '../../assets/Logo.png'
+import loginUsuario from '../../assets/loginUsuario.png'
+import loginCadeado from '../../assets/loginCadeado.png'
+import { Card, Button } from 'react-bootstrap';
+import "./Login.css"
+
 
 const Login = () => {
   return (
-    <>Login</>
+    /*<div className="card">
+      <img className='logo' src={Logo} alt="Logo" />
+      <div className="card-content">
+        <h2>Pronto para ficar frio?</h2>
+        <div className="caixa1">Senha</div>
+        <div className="caixa1">Senha</div>
+        <div className="caixa2">Esqueceu sua senha?</div>
+        <Button>Continuar</Button>
+        <p className='link_cadastro'>Não possui conta? <a href="">Clique aqui e faça seu cadastro</a></p>
+      </div>
+    </div >*/
+
+    <div className="fundo">
+      <Card className="text-center">
+        
+        <Card.Link href="/" className='logo'>
+          <img
+            src={Logo}
+            alt="Logo"
+            width={100}
+          />
+        </Card.Link>
+        
+        <Card.Title><strong>Pronto para ficar frio?</strong></Card.Title>
+
+        <Card body className='senha'>
+          <img
+            className='loginUsuario'
+            src={loginUsuario}
+            alt="loginUsuario"
+          />
+          Senha
+        </Card>
+
+        <Card body className='senha'>
+          <img
+            className='loginCadeado'
+            src={loginCadeado}
+            alt="loginCadeado"
+          />
+          Senha
+        </Card>
+
+        <Button className='esqueceuSenha'>
+          <strong>Esqueceu sua senha?</strong>
+        </Button>
+
+        <Button className='continuar'>
+          <strong>Continuar</strong>
+        </Button>
+
+        <Card.Text className='naoPossuiConta'>
+          Não possui conta? -
+          <Card.Link href="cadastro">
+            Clique aqui e faça seu cadastro
+          </Card.Link>
+        </Card.Text>
+
+      </Card>
+    </div>
   )
 }
 
- export default Login
-
+export default Login
